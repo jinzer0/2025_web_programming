c6d377a17ee6c5f4b2464b9baf12af9cc2a3dbcb
Kim Jin-yeong <jinzero29@gmail.com>
2025-05-18 23:46:41 +0900

    Node js lecture - Add more middleware in app.js

--- web/source/app.js
+++ web/source/app.js
@@ -1,12 +1,18 @@
 let express = require("express");
 let http = require("http");
+let path = require("path");
 let static_s = require("serve-static");
 
 let app = express();
+let router = express.Router();
 app.set("port", process.env.PORT || 8080);
 app.set("host", "127.0.0.1");
 app.use(static_s(__dirname));
 
+app.use(express.urlencoded());
+app.use(express.json());
+
 http.createServer(app).listen(app.get("port"), app.get("host"), () => {
     console.log("Express server running at " + app.get("port") + app.get("host"));
-})
+});
+
