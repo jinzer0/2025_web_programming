c5888f82eca6b1c0691d3e537635c2ccd13cdf08
Kim Jin-yeong <jinzero29@gmail.com>
2025-05-26 19:22:46 +0900

    Break out - Work in Progress score.js

---
+++ break_out/js/score.js
@@ -0,0 +1,15 @@
+let storage = window.localStorage;
+let result = [];
+// 프로필 정렬 -
+function sortData() {
+
+}
+
+let sto = {
+    profile1: {
+        user_name: "kch",
+        highest_score: "200",
+        longest_survived: 2020,
+        average_survived: 1000,
+    },
+}
