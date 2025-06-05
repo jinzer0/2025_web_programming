9188f15fcbee18989bdee7f61762604e45c00d5d
Kim Jin-yeong <jinzero29@gmail.com>
2025-06-05 18:11:36 +0900

    Break out - Implement dynamic score table generation and integrate profile data

--- break_out/js/score.js
+++ break_out/js/score.js
@@ -1,15 +1,51 @@
 let storage = window.localStorage;
 let result = [];
 // 프로필 정렬 -
+
+$(() => {
+    sortData();
+});
+
 function sortData() {
+    let profile = profileManager.getAllProfiles();
+    let table=$("<table/>");
+    let thead=$("<thead/>");
+    let first_row=$("<tr/>").append(
+        $("<th/>").text("User name"),
+        $("<th/>").text("Highest score"),
+        $("<th/>").text("Longest survived time"),
+        $("<th/>").text("Average survived time")
+    );
+    thead.append(first_row);
+    let tbody=$("<tbody/>");
+    let second_row_tr=$("<tr/>");
+    let second_row_td=$("<td/>").attr({
+        "colspan":"4",
+        "style":"color:lightgrey"
+    });
+    second_row_td.text("상위 10개 기록만 표시 됩니다.");
+    second_row_tr.append(second_row_td);
+    tbody.append(second_row_tr);
+
+    for(let i=0;i<profile.length;i++){
+        let user_name=profile[i].name;
+        let user_Highest_score=profile[i].highest_score;
+        let user_Longest_time=profile[i].longest_survived_time;
+        let user_average_time=profile[i].average_survived_time;
+        var row=$("<tr/>").append(
+            $("<td/>").text(user_name),
+            $("<td/>").text(user_Highest_score),
+            $("<td/>").text(user_Longest_time),
+            $("<td/>").text(user_average_time)
+        );
+        tbody.append(row);
+    }
+    table.append(thead);
+    table.append(tbody);
+
+    $("#score-screen-middle-container").append(table);
+
 
 }
 
-let sto = {
-    profile1: {
-        user_name: "kch",
-        highest_score: "200",
-        longest_survived: 2020,
-        average_survived: 1000,
-    },
-}
+
